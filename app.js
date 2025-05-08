f444a75d8764c5ed1ba47cfad5b373dc1aa2f261
Paresa288 <parehacker99@gmail.com>
2025-05-08 21:02:13 +0200

    class

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-const grid = document.querySelector('.grid')
-const scoreDisplay = document.getElementById('score')
-const width = 8
-const squares = []
-let score = 0
+  const grid = document.querySelector('.grid')
+  const scoreDisplay = document.getElementById('score')
+  const width = 8
+  const squares = []
+  let score = 0
 
-const candyColors = [
+  const candyColors = [
     'url(images/red-candy.png)',
     'url(images/yellow-candy.png)',
     'url(images/orange-candy.png)',
@@ -15,31 +15,31 @@ const candyColors = [
   ]
 
 //create your board
-function createBoard() {
+  function createBoard() {
   for (let i = 0; i < width*width; i++) {
-    const square = document.createElement('div')
-    square.setAttribute('draggable', true)
-    square.setAttribute('id', i)
-    let randomColor = Math.floor(Math.random() * candyColors.length)
-    square.style.backgroundImage = candyColors[randomColor]
-    grid.appendChild(square)
-    squares.push(square)
+      const square = document.createElement('div')
+      square.setAttribute('draggable', true)
+      square.setAttribute('id', i)
+      let randomColor = Math.floor(Math.random() * candyColors.length)
+      square.style.backgroundImage = candyColors[randomColor]
+      grid.appendChild(square)
+      squares.push(square)
+    }
   }
-}
-createBoard()
+  createBoard()
 
 // Dragging the Candy
-let colorBeingDragged
-let colorBeingReplaced
-let squareIdBeingDragged
-let squareIdBeingReplaced
+  let colorBeingDragged
+  let colorBeingReplaced
+  let squareIdBeingDragged
+  let squareIdBeingReplaced
 
-squares.forEach(square => square.addEventListener('dragstart', dragStart))
-squares.forEach(square => square.addEventListener('dragend', dragEnd))
-squares.forEach(square => square.addEventListener('dragover', dragOver))
-squares.forEach(square => square.addEventListener('dragenter', dragEnter))
-squares.forEach(square => square.addEventListener('drageleave', dragLeave))
-squares.forEach(square => square.addEventListener('drop', dragDrop))
+  squares.forEach(square => square.addEventListener('dragstart', dragStart))
+  squares.forEach(square => square.addEventListener('dragend', dragEnd))
+  squares.forEach(square => square.addEventListener('dragover', dragOver))
+  squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+  squares.forEach(square => square.addEventListener('drageleave', dragLeave))
+  squares.forEach(square => square.addEventListener('drop', dragDrop))
 
 function dragStart(){
     colorBeingDragged = this.style.backgroundImage
